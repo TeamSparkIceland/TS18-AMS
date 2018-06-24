efd542a521c13246660e0b3ec9266c0b7c572b21
KristjanOrn <orninn96@gmail.com>
2018-06-24 17:45:47 +0000

    Initial commit

---
+++ sketch_BMS/BMS.h
@@ -0,0 +1,73 @@
+/*!
+  Name:         BMS.h
+  Date created: 2.5.18
+  Version:      2.0 (16.06.18)
+  Description:  Contains functions for reading and working with data from LTC6811 using library for LTC68041
+*/
+
+#ifndef BMS_H
+#define	BMS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// Define wether to print out and excecute debug commands
+#define BMS_debug  true
+
+#define TOTAL_SENSORS 12
+#define TEMP_COUNT 5
+#define VOLTAGE_COUNT 5
+
+//#define TOTAL_IC  12  // number of ICs in the daisy chain
+
+
+// Structure for containing values for each individual cell
+typedef struct Cells {
+  float voltage;
+  float temperature;
+  bool voltage_pec_failure;
+  bool temperature_pec_failure;
+  uint8_t voltage_error_count;
+  uint8_t temperature_error_count;
+  bool discharge_enabled;
+} Cell;
+
+typedef struct dischargeState {
+  uint8_t state;
+} DischargeState;
+
+
+#define BMS_low_voltage      3.2   // V
+#define BMS_high_voltage     4.18  // V
+#define BMS_low_temperature  5.0   // Degrees C
+#define BMS_high_temperature 58.0  // Degrees C
+
+
+
+// Function declarations
+void BMS_Initialize();
+bool BMS_check();
+bool BMS_is_error();
+
+uint16_t BMS_get_error_code();
+
+void BMS_set_thresholds(float cell_low, float cell_high, float sensor_low, float sensor_high);
+
+void BMS_test_stuff();
+
+bool BMS_is_discharge_enabled();
+float BMS_get_target_voltage();
+void BMS_set_discharge(bool state);
+void BMS_handle_discharge();
+void BMS_clear_discharge();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* BMS_H */
---
+++ sketch_BMS/sketch_BMS.ino
@@ -0,0 +1,123 @@
+/*!
+  Team spark BMS
+  Name:         sketch_BMS.ino
+  Date created: 27.5.18
+  Version:      2.0 (16.06.18)
+  Description:
+
+  @verbatim
+
+  NOTES
+  Setup:
+   Set the terminal baud rate to 115200 and select the newline terminator.
+   Ensure all jumpers on the demo board are installed in their default positions from the factory.
+   Refer to Demo Manual D1894B.
+
+  USER INPUT DATA FORMAT:
+  decimal : 1024
+  hex     : 0x400
+  octal   : 02000  (leading 0)
+  binary  : B10000000000
+  float   : 1024.0
+  @endverbatim
+
+*/
+
+#define IO_TSAL_ENABLE_GetValue()  digitalRead(0)
+
+
+#define PERIOD 0x0001
+#define ZERO 0x0000
+
+#define DISCHARGE_CYCLE 60
+#define REST_CYCLE 30
+int TMR0IF;
+
+// Libraries
+#include <Arduino.h>
+#include <stdint.h>
+#include "Linduino.h"
+#include "LT_SPI.h"
+#include "UserInterface.h"
+#include "LTC68041.h"
+#include <SPI.h>
+// Other files
+#include "BMS.h"
+
+
+uint8_t counter = 1;
+bool reverse = false;
+uint8_t timer_counter;
+uint8_t inbound_data;
+
+bool enable_discharge = false;
+
+bool discharge_rest_period = false;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(IO_BMS_CS, OUTPUT);
+  IO_BMS_CS_SetHigh();
+  BMS_Initialize();
+  TMR0IF = 0;
+}
+
+void loop()
+{
+  Serial.println("Loop");
+  delay(50);
+  
+  // Check data for tolerance levels
+  if (BMS_check() == true) {
+    Serial.print("E|BMS Shutdown triggered|\r\n");
+    IO_BMS_CS_SetLow();
+  } else {
+    IO_BMS_CS_SetHigh();
+  }
+  if ((enable_discharge) && (!BMS_is_discharge_enabled())) {
+    BMS_set_discharge(true);
+    Serial.print("Discharge Voltage: ");
+    Serial.println((uint16_t)(BMS_get_target_voltage() * 1000));
+  } else if ((!enable_discharge) && (BMS_is_discharge_enabled())) {
+    BMS_set_discharge(false);
+  }
+
+  if (TMR0IF > 0) {
+    timer_counter--;
+    if (timer_counter == 0) {
+      discharge_rest_period = !discharge_rest_period;
+      if (discharge_rest_period) {
+        timer_counter = REST_CYCLE;
+      } else {
+        timer_counter = DISCHARGE_CYCLE;
+      }
+    }
+
+    TMR0IF = 0;
+
+    // Command detection
+    if (Serial.available() > 0) {
+      inbound_data = Serial.read();
+      switch (inbound_data) {
+        case 'D':
+          enable_discharge = false;
+          break;
+        case 'E':
+          enable_discharge = true;
+          break;
+      }
+    }
+  }
+
+  if (discharge_rest_period == false) {
+    BMS_handle_discharge();
+  } else {
+    BMS_clear_discharge();
+  }
+}
+
+
+
+
