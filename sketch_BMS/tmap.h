1b172eef6f5db7922c690ac27c64c8d494b4285c
tar <tar3@hi.is>
2018-06-26 21:44:37 +0000

    changed to polynomial

--- sketch_BMS/tmap.h
+++ sketch_BMS/tmap.h
@@ -1,9 +1,9 @@
 #ifndef TMAP_H
 #define	TMAP_H
 
-// Temperature = Index / 2 (Tempreatures in 0.5 °C increments)
+// Temperature = Index / 2 (Tempreatures in 0.5 ï¿½C increments)
 // Voltage = Value
-extern const float temperature_map[201];
+//extern const float temperature_map[201];
 
 float LookupTemperature(float voltage);
 
